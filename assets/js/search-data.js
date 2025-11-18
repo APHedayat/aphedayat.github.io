40d83f9c952badd1d07dfb424b9f268b431f8b1b
APHedayat <62061917+APHedayat@users.noreply.github.com>
2025-11-18 15:22:39 +0000

    Deploying to gh-pages from @ APHedayat/aphedayat.github.io@877b92a145d266c6b513f3e2e113f5076131da3b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/https:/aphedayat.github.io/assets/pdf/aphedayat_cv.pdf";
+            window.location.href = "/https:/aphedayat.github.io/assets/pdf/amirpasha_hedayat___cv.pdf";
           },
         },{id: "post-my-first-post",
         
